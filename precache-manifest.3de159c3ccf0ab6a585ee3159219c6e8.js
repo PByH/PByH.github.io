540c0ab5cdfc76f97e528d861292cf49e24fe89d
PByH <pbyh_kpyt@mail.ru>
2021-12-29 13:59:28 +0700

    first build

---
+++ precache-manifest.3de159c3ccf0ab6a585ee3159219c6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867caeb29213db206b4380779005d6be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f34809c3d73e4f2782fd",
+    "url": "/static/css/main.d057795d.chunk.css"
+  },
+  {
+    "revision": "2faedaf8a6b9963db754",
+    "url": "/static/js/2.54b3449e.chunk.js"
+  },
+  {
+    "revision": "41e8aedee29e6e7a9a1bdd0a210ba283",
+    "url": "/static/js/2.54b3449e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34809c3d73e4f2782fd",
+    "url": "/static/js/main.000c8a58.chunk.js"
+  },
+  {
+    "revision": "2a3496e0349049e73fa1",
+    "url": "/static/js/runtime-main.9aef5679.js"
+  }
+]);
